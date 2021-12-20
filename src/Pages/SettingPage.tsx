3c7ac1d5b0b27865b2f2fc60363f7cc3767fab1d
Ted Zyzsdy <zyzsdy@gmail.com>
2021-12-20 17:25:27 +0800

    add settings page template

--- src/Pages/SettingPage.tsx
+++ src/Pages/SettingPage.tsx
@@ -1,4 +1,4 @@
-import { Divider, Form, Select } from 'antd';
+import { Divider, Form, Input, InputNumber, Select, Switch } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './SettingPage.scss';
@@ -17,11 +17,42 @@ export default function SettingPage() {
             <h1>{t('SettingsTitle')}</h1>
             <Divider />
             <div>
+                <h2>{t('General')}</h2>
                 <Form labelCol={{span: 4}} wrapperCol={{span: 14}} layout="horizontal">
                     <Form.Item label={t('Language')}>
                         <Select defaultValue={i18n.language} onChange={langChange}>
                             {langList.map(l => (<Select.Option key={l} value={l}>{t(l, {ns: "lang"})}</Select.Option>))}
                         </Select>
+                    </Form.Item>
+                </Form>
+                <h2>{t('MinyamiOptions')}</h2>
+                <Form labelCol={{span: 4}} wrapperCol={{span: 14}} layout="horizontal">
+                    <Form.Item label={t('Threads')}>
+                        <InputNumber defaultValue={5} />
+                    </Form.Item>
+                    <Form.Item label={t('Retries')}>
+                        <InputNumber defaultValue={5} />
+                    </Form.Item>
+                    <Form.Item label={t('Headers')}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label={t('Format')}>
+                        <Select defaultValue="ts">
+                            <Select.Option value="ts">ts</Select.Option>
+                            <Select.Option value="mkv">mkv</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label={t('Nomerge')}>
+                        <Switch />
+                    </Form.Item>
+                    <Form.Item label={t('Verbose')}>
+                        <Switch />
+                    </Form.Item>
+                </Form>
+                <h2>{t('Network')}</h2>
+                <Form labelCol={{span: 4}} wrapperCol={{span: 14}} layout="horizontal">
+                    <Form.Item label={t('Proxy')}>
+                        <Input />
                     </Form.Item>
                 </Form>
             </div>
